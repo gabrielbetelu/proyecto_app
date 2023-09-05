47bef5226cd6430b3cd2175d01d356e5df34bf58
Gabriel Betelu <gabrielbetelu@yahoo.com.ar>
2023-09-05 16:24:26 -0300

    creo GenreCard, modifico GerresInDb usando GenreCard y creo array genresInDb

---
+++ src/components/GenreCard.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types';;
+
+function GenreCard(props) {
+
+
+    return (
+        <React.Fragment>
+            {/*<!{/*-- Movies in Data Base -->*/}
+            <div className="col-lg-6 mb-4">
+                <div className="card bg-dark text-white shadow">
+                    <div className="card-body">
+                        {props.genre}
+                    </div>
+                </div>
+            </div>
+        </React.Fragment>
+        )
+}
+
+export default GenreCard
+
+
+GenreCard.propTypes = {
+    atributes: PropTypes.shape({
+        genre: PropTypes.string.isRequired        
+    })
+}
+
+GenreCard.defaultProps = {
+    genre: "Sin Genero"
+}
+  
--- src/components/GenresInDb.js
+++ src/components/GenresInDb.js
@@ -1,4 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
+import GenreCard from "./GenreCard";
+
+let genresInDb = [
+    {
+        id: 1,
+        genre: "Acción"
+    },
+    {
+        id: 2,
+        genre: "Animación"
+    },
+    {
+        id: 3,
+        genre: "Aventura"
+    },
+    {
+        id: 4,
+        genre: "Ciencia Ficción"
+    },
+    {
+        id: 5,
+        genre: "Comedia"
+    },
+    {
+        id: 6,
+        genre: "Documental"
+    },
+    {
+        id: 7,
+        genre: "Drama"
+    },
+    {
+        id: 8,
+        genre: "Fantasía"
+    },
+    {
+        id: 9,
+        genre: "Infantiles"
+    },
+    {
+        id: 10,
+        genre: "Musical"
+    }
+]
+
 
 function GenresInDb() {
 
@@ -13,76 +58,8 @@ function GenresInDb() {
                     </div>
                     <div className="card-body">
                         <div className="row">
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Acción
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Animación
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Aventura
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Ciencia Ficción
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Comedia
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Documental
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Drama
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Fantasia
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Infantiles
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-6 mb-4">
-                                <div className="card bg-dark text-white shadow">
-                                    <div className="card-body">
-                                        Musical
-                                    </div>
-                                </div>
-                            </div>
+
+                        {genresInDb.map((raw ,i) => <GenreCard key= {raw.genre + i} {...raw}/>)}
 
                         </div>
                     </div>
